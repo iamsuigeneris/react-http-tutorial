d8e970d876daf713847445cbc0d61ca82ae3fe54
iamsuigeneris <irewolelanre@gmail.com>
2021-01-12 19:32:45 +0100

    react http lesson

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './App.css';
+import PostForm from './components/PostForm';
+import PostList from './components/PostList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PostForm />
+        {/* <PostList /> */}
+      </div>
+    )
+  }
+}
+
+export default App
+
+
+
